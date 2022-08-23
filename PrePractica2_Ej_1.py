ddf5a3788e54d191d3e1039258ad30bed4aa57c9
Mora Capdevila <moracapdevila457@gmail.com>
2022-08-23 15:49:01 -0300

    Primer ej completo

--- PrePractica2_Ej_1.py
+++ PrePractica2_Ej_1.py
@@ -3,6 +3,20 @@
 #Nota : Hacerlo con la función max(a,b) y luego con una comparación
 
 #INICIO
+lista = []
+a = int(input("Ingresar el valor de a: "))
+b = int(input("Ingresar el valor de b: "))
+lista.append(a)
+lista.append(b)
+max_valor = max(lista)
+print("El maximo valor es: ", max_valor)
+
+if lista[0] > lista[1] :
+    max_valor = lista[0]
+else:
+    max_valor = lista[1]
+
+print("El maximo valor es: " , max_valor)
 
 
 #FIN
