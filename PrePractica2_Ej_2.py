075f6aedb9025f2be7f1a3936d1d7f781d06fc9b
Francisco Carballo <francisco.carballo.fc@gmail.com>
2022-08-23 20:05:24 -0300

    Ejc2 resuelto

--- PrePractica2_Ej_2.py
+++ PrePractica2_Ej_2.py
@@ -3,5 +3,18 @@
 #El programa debe de mostrar por pantalla la lista de numeros originales y la lista de numeros impares.
 
 #INICIO
+lista = []
+lista_impar = []
 
+("Ingrese la cantidad de numeros de la lista")
+n = int(input("Ingrese la cantidad de numeros de la lista: "))
+
+for i in range(n) :
+    num=int(input("Ingrese un numero: "))
+    lista.append(num)
+    if num%2 != 0:
+     lista_impar.append(num)  
+
+print("La lista entera tiene los siguientes valores :",lista)
+print("Los valores impares son :",lista_impar)   
 #FIN
